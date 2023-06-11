db1846e8cfc15017bd31ac69a1e692e4e49251e4
joncherston <joncherston@users.noreply.github.com>
2023-06-11 19:19:48 -0400

    Update install.sh
    Added python-smbus, as well as python3-smbus, libi2c-dev, playsound, in attempts to correct memory leaks and buffer underruns

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt-get -y upgrade
 sudo apt-get install -y python3 python3-pip 
-sudo apt-get install -y libsdl1.2-dev libsdl-mixer1.2 libsdl2-mixer-2.0-0
+sudo apt-get install -y libsdl1.2-dev libsdl-mixer1.2 libsdl2-mixer-2.0-0 libi2c-dev
 sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel
 sudo apt-get install -y python-smbus
 sudo apt-get install -y python3-smbus
@@ -8,4 +8,6 @@ sudo pip3 install --upgrade setuptools
 sudo pip3 install RPI.GPIO
 sudo pip3 install PySDL2 pygame nfcpy
 sudo pip3 install configobj httplib2
+sudo pip install pygame
+sudo pip install playsound
 amixer set PCM 90%
