35aa02a794e41ed98b2018b2ba2e5b1eef92aa55
joncherston <joncherston@users.noreply.github.com>
2023-07-01 11:17:46 -0400

    Update install.sh
    Modified install.sh, to remove buffer underpin and memory leak errors. In order to do this, it replaces alsa pulse audio with pygame audio and installs python3-smbus instead of python-smbus since it can no longer be found.

--- install.sh
+++ install.sh
@@ -1,13 +1,8 @@
 sudo apt-get -y upgrade
+sudo apt-get install -y python3-pygame
 sudo apt-get install -y python3 python3-pip 
-sudo apt-get install -y libsdl1.2-dev libsdl-mixer1.2 libsdl2-mixer-2.0-0 libi2c-dev
 sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel
-sudo apt-get install -y python-smbus
 sudo apt-get install -y python3-smbus
 sudo pip3 install --upgrade setuptools
-sudo pip3 install RPI.GPIO
 sudo pip3 install PySDL2 pygame nfcpy
 sudo pip3 install configobj httplib2
-sudo pip install pygame
-sudo pip install playsound
-amixer set PCM 90%
